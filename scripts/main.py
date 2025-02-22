6cf6e4f1728239cd4d6a6f4ad34ae5488181f8b9
Fabio Rueda <avances123@gmail.com>
2025-02-22 13:20:58 +0100

    ciclo entre teorias

--- scripts/main.py
+++ scripts/main.py
@@ -1,26 +1,44 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Digits, Static, OptionList, ContentSwitcher, MarkdownViewer
+from textual.widgets import Footer, Header, Digits, Static, OptionList, ContentSwitcher, MarkdownViewer, Markdown
 from textual.containers import Horizontal, Vertical, VerticalScroll, HorizontalGroup, VerticalGroup
 from textual.widgets import Input, Label
 from textual.screen import Screen
+from textual.message import Message
 from textual.reactive import reactive
 
 from pyscuba.calculators.nitrox_calculator import NitroxCalculator
 from pyscuba.physics.depth_converter import DepthConverter
 
 
-class Teoria(MarkdownViewer):
+class Teoria(Markdown):
+    can_focus = False
+
     def __init__(self, file_path: str, *args, **kwargs) -> None:
         # Lee el contenido del archivo
         with open(file_path, 'r') as file:
             self.markdown_content = file.read()
-        kwargs["show_table_of_contents"] = False
         super().__init__(self.markdown_content, *args, **kwargs)
 
+    def update_content(self, new_file_path: str) -> None:
+        """Método para actualizar el contenido de la teoría según el archivo proporcionado."""
+        with open(new_file_path, 'r') as file:
+            self.markdown_content = file.read()
+        self.update(self.markdown_content)
+
 class CajaCalculos(Static):
-    pass
+    def on_descendant_focus(self):
+        self.add_class("focused")
+                # Determinar qué tipo de caja tiene el foco y actualizar la teoría
+        if isinstance(self, MOD):
+            self.app.get_widget_by_id("teoria").update_content("theory/mod.md")
+        elif isinstance(self, EAD):
+            self.app.get_widget_by_id("teoria").update_content("theory/ead.md")
+
+    def on_descendant_blur(self):
+        self.remove_class("focused")
         
 class FilaCalculo(Static):
+        
     def __init__(self, label: str, placeholder="Inserta el dato...") -> None:
         self.label = label
         self.placeholder = placeholder
@@ -31,6 +49,7 @@ class FilaCalculo(Static):
             yield Label(self.label, classes="label-variable")
             self.input = Input(type="number", placeholder=self.placeholder)
             yield self.input
+
 
 class ResultadoCalculo(Static):
 
@@ -67,17 +86,40 @@ class MOD(CajaCalculos):
             valor2 = float(self.input2.input.value) if self.input2.input.value else 0
         except ValueError:
             valor1 = valor2 = 0  # Si hay un valor no numérico
-
-
         d = DepthConverter.for_salt_water(0) # Agua del mar
         n = NitroxCalculator(d)
         try:
             mod = n.mod(valor1, valor2)
         except (ZeroDivisionError, ValueError):
             mod = 0
-        
-
         self.resultado.resultado = mod  # Actualiza el resultado
+
+
+class EAD(CajaCalculos):
+    def compose(self) -> ComposeResult:
+        with HorizontalGroup():
+            self.input1 = FilaCalculo(label="Max ppO2 (bar)", placeholder="Ej. 1.6")
+            self.input2 = FilaCalculo(label="O2 Fraction (%)")
+            yield self.input1
+            yield self.input2
+        self.resultado = ResultadoCalculo("MOD (m)")
+        yield self.resultado
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        """Calcula el nuevo resultado cuando cambia un input."""
+        try:
+            valor1 = float(self.input1.input.value) if self.input1.input.value else 0
+            valor2 = float(self.input2.input.value) if self.input2.input.value else 0
+        except ValueError:
+            valor1 = valor2 = 0  # Si hay un valor no numérico
+        d = DepthConverter.for_salt_water(0) # Agua del mar
+        n = NitroxCalculator(d)
+        try:
+            mod = n.mod(valor1, valor2)
+        except (ZeroDivisionError, ValueError):
+            mod = 0
+        self.resultado.resultado = mod  # Actualiza el resultado
+
 
 
 class CalculatorScreen(Screen):
@@ -85,8 +127,10 @@ class CalculatorScreen(Screen):
         with HorizontalGroup():
             with VerticalGroup():
                 yield (mod := MOD())
+                yield (ead := EAD())
             yield (teoria := Teoria("theory/mod.md", id="teoria"))
         mod.border_title = "MOD"
+        ead.border_title = "EAD"
         teoria.border_title = "Teoria"
 
 class PyScubaApp(App):
---
+++ theory/ead.md
@@ -0,0 +1,31 @@
+# Equivalent air depth
+
+The equivalent air depth (EAD) is a way of approximating the decompression requirements of breathing gas mixtures that contain nitrogen and oxygen in different proportions to those in air, known as nitrox.
+
+The equivalent air depth, for a given nitrox mix and depth, is the depth of a dive when breathing air that would have the same partial pressure of nitrogen. So, for example, a gas mix containing 36% oxygen (EAN36) being used at 27 metres (89 ft) has an EAD of 20 metres (66 ft).
+
+## Calculation in metres
+The equivalent air depth can be calculated for depths in metres as follows:
+
+EAD = (Depth + 10) × (Fraction of N2 / 0.79) − 10
+Working the earlier example, for a nitrox mix containing 64% nitrogen (EAN36) being used at 27 metres, the EAD is:
+
+EAD = (27 + 10) × (0.64 / 0.79) − 10
+EAD = 37 × 0.81 − 10
+EAD = 30 − 10
+EAD = 20 metres
+So at 27 metres on this mix, the diver would calculate their decompression requirements as if on air at 20 metres.
+
+
+## Calculations in feet
+The equivalent air depth can be calculated for depths in feet as follows:
+
+EAD = (Depth + 33) × (Fraction of N2 / 0.79) − 33
+Working the earlier example, for a nitrox mix containing 64% nitrogen (EAN36) being used at 90 feet, the EAD is:
+
+EAD = (90 + 33) × (0.64 / 0.79) − 33
+EAD = 123 × 0.81 − 33
+EAD = 100 − 33
+EAD = 67 feet
+So at 90 feet on this mix, the diver would calculate their decompression requirements as if on air at 67 feet.
+
